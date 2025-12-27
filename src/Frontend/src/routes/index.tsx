0677841358a06ba09d5a5875e4e9d6e32952a0a3
Fernando <fgoya70@gmail.com>
2025-12-27 20:36:34 -0300

    frontend

---
+++ src/Frontend/src/routes/index.tsx
@@ -0,0 +1,13 @@
+//Revisar
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// export function AppRoutes() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/login" element={<Login />} />
+//         <Route path="/dashboard" element={<Dashboard />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
